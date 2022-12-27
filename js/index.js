aaafd0349ceb54a2028a476ca7c180ebcb5090d6
Sangel Ramirez <106925886+Sangelrm@users.noreply.github.com>
2022-12-27 16:31:40 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cartItems.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItems.classList.remove('active');
+}
+
+let cartItems = document.querySelector('.cart-items-container');
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItems.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItems.classList.remove('active');
+}
